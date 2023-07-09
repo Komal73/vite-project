80d325e9a3f5ac5151a30862023fe8e2ff5cb934
yogesh negi <yogesh@locad.net>
2023-07-09 12:43:51 +0530

    nav-bar

---
+++ src/Components/Nav-Bar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './NavBar.css'
+const NavBar = () => {
+  return (
+    <div className='main-nav-bar'>
+        <div className='Logo'>
+            <a href='/'>
+                <img src='logo.webp' alt='logo' style={{height:"160%", width:"100%"}}/>
+            </a>
+        </div>
+
+        <div className='SignIn'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/Pages/Home Page/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './HomePage.css'
+import NavBar from '../../Components/Nav-Bar/NavBar'
+const HomePage = () => {
+  return (
+    <>
+    <NavBar/>
+    </>
+  )
+}
+
+export default HomePage

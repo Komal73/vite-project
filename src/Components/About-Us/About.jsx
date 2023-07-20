eb4cfd1e4494c8947f4eb4d73320cf33ba4faadc
Yogesh Negi <113903081+itsyogeshnegi@users.noreply.github.com>
2023-07-20 19:43:06 +0530

    About PAge

--- src/Components/About-Us/About.jsx
+++ src/Components/About-Us/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <>  
     <div className='about-section'>
-        <div className='about-heading'><h2>About Us</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>Sint recusandae consectetur velit magni, rerum assumenda deleniti aliquam quo ad omnis?</p></div>
+        <div className='about-heading'><h2>About Us</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Sint recusandae consectetur velit magni, <br/> rerum assumenda deleniti aliquam quo ad omnis?</p></div>
     </div>
     <AboutVideo />
     </>

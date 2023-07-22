2fd9e8047f64eb52581c719f640a0a753c7faeca
Yogesh Negi <113903081+itsyogeshnegi@users.noreply.github.com>
2023-07-22 13:18:45 +0530

    push

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,28 +1,24 @@
 import React from 'react'
-
+import './Blog.css'
 const Blog = () => {
     return (
         <>
             <div className="blog-section">
                 <div className="blog-heading">
-                    <h2>Blog</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi maxime sunt voluptatum id magnam obcaecati doloribus doloremque maiores repellendus, sapiente eum laborum quaerat odio impedit mollitia vel minus error culpa!</p>
+                    <h2>BLOG</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br/>Eligendi maxime sunt voluptatum id magnam.</p>
                 </div>
                 <div className="blog-content">
                     <div className="blog-box">
-                        <div className="blog-btn">
-                            <button>Read More</button>
-                        </div>
+                    <img src="indian food.jpg"  style={{height:'100%' , width:'100%' , }} />
+                    </div>
+                    <div className="blog-box btns">
+                        <h5>Lorem ipsum dolor sit,<br/> amet consectetur adipisicing.</h5>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur cumque iste iure est dignissimos dolorum.</p>
+                            <button id='iiii'>Read More</button>
                     </div>
                     <div className="blog-box">
-                        <div className="blog-btn">
-                            <button>Read More</button>
-                        </div>
-                    </div>
-                    <div className="blog-box">
-                        <div className="blog-btn">
-                            <button>Read More</button>
-                        </div>
+                    <img src="japanese.jpg"  style={{height:'100%' , width:'100%' , }} />
                     </div>
                 </div>
             </div>
